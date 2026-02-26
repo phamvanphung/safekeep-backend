1c403b2205526c6c10c6f299409b44b9b08b4490
Phạm Văn Phụng <phamvanphung1999@gmail.com>
2026-02-26 19:07:42 +0700

    update config domain

--- SAFEKEEP-FE/vite.config.ts
+++ SAFEKEEP-FE/vite.config.ts
@@ -5,7 +5,9 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 5173,
+    allowedHosts: true,
   },
   preview: {
     port: 4173,
